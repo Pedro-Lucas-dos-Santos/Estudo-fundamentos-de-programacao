68554f96dca3aa4fe17cc4ae7355c17e046962fc
Pedro Lucas <lukkas.78@outlook.com>
2024-02-05 19:06:01 -0300

    resolvendo exercicios 5 e 6

---
+++ ex05.js
@@ -0,0 +1,5 @@
+function Soma(a,b) {
+    return console.log(a + b)
+}
+
+Soma(3, 5)
---
+++ ex06.js
@@ -0,0 +1,16 @@
+let pessoas = [
+    {
+        nome: "Pedro",
+        idade: "22",
+        cidade: "Campo Alegre"
+    },
+    {
+        nome: "Lucas",
+        idade: "23",
+        cidade: "Campo Alegre"
+    }
+]
+
+pessoas[1].profissao = "Desenvolvedor";
+
+console.log(pessoas)
