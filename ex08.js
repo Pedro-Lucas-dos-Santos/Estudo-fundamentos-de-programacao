fd9b3b97f52e845d29f53f93f7ec8681cab60dd7
Pedro Lucas <lukkas.78@outlook.com>
2024-02-11 13:49:23 -0300

    exercicio 08

---
+++ ex08.js
@@ -0,0 +1,7 @@
+//07 - Crie uma função que recebe um nome como parâmetro e imprime uma saudação personalizada.
+
+function saudacao(nome) {
+    console.log(`Olá, ${nome}! como você está?`)
+}
+
+saudacao("Pedro")
