17a91d7d220830699a699e8dfa92012f9c5a38aa
Pedro Lucas <lukkas.78@outlook.com>
2024-02-12 14:14:05 -0300

    exercicio 09

---
+++ ex09.js
@@ -0,0 +1,9 @@
+//09 - Crie um programa que verifica se um número é par ou ímpar.
+
+let numero = parseInt(prompt('Digite um número'))
+
+if(numero % 2 === 0) {
+    alert("O numero é par")
+} else {
+    alert("O número é ímpar")
+}
