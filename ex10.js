c0d256539a3d053c4c8a3b3ecf452cea0c6d06bb
Pedro Lucas <lukkas.78@outlook.com>
2024-02-12 14:26:01 -0300

    exercicio 10

---
+++ ex10.js
@@ -0,0 +1,9 @@
+//09 - Crie um programa que recebe o ano de nascimento de uma pessoa e o ano atual, calcule e mostre
+
+let anoAtual = parseInt(prompt("Digite o ano atual"))
+
+let anoDeNascimento = parseInt(prompt("Digite o ano em que você nasceu"))
+
+let resultado = anoAtual - anoDeNascimento
+
+alert("Você tem " +resultado+ " anos")
